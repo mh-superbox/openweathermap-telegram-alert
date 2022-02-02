703c0c6d8ce0f6d0a70d4548f99bbcea15c9e0dc
Michael Hacker <mh@superbox.one>
2022-02-02 21:11:04 +0100

    Init

---
+++ README.md
@@ -0,0 +1,53 @@
+# OpenWeatherMap Telegram Alert
+
+Send OpenWeatherMap alerts (One Call API) to telegram users.
+
+## Installation
+
+**Requirements:**
+
+```shell
+$ apt install python3-requests
+```
+
+Copy the `om-telegram-alert.py` file:
+
+```shell
+$ cp src/usr/local/sbin/om-telegram-alert.py /usr/local/sbin/om-telegram-alert.py
+$ chmod +x /usr/local/sbin/om-telegram-alert.py
+```
+
+Copy the `om-telegram-alert.yaml` configuration file:
+
+```shell
+$ cp src/etc/default/om-telegram-alert.yaml /etc/default
+```
+
+## Configuration
+
+You can set OpenWeatherMap and Telegram settings in the `/etc/default/om-telegram-alert.yaml`.
+
+### OpenWeaterMap
+
+| Key     | Value                                            |
+|---------|--------------------------------------------------|
+| `lat`   | latitude - Geographical coordinates (required).  |
+| `lon`   | longitude - Geographical coordinates (required). |
+| `appid` | Your unique API key (required).                  |
+
+### Telegram
+
+| Key         | Value                           |
+|-------------|---------------------------------|
+| `token`     | Bot token (required).           |
+| `chat_ids`  | User chat ids (required).       |
+
+
+## Start systemd timer
+
+The timer check every 15 minutes for new entries.
+
+```shell
+cp src/etc/systemd/system/om-telegram-alert.* /etc/systemd/system
+systemctl enable --now om-telegram-alert.timer
+```
---
+++ src/usr/local/sbin/om-telegram-alert.py
@@ -0,0 +1,147 @@
+#!/usr/bin/env python3
+import json
+import logging
+from dataclasses import is_dataclass, dataclass, field
+from pathlib import Path
+from tempfile import gettempdir
+from typing import Final, List
+
+import requests
+import yaml
+from datetime import datetime
+
+OM_ONECALL_URL: Final[str] = "https://api.openweathermap.org/data/2.5/onecall"
+TELEGRAM_SEND_MSG_URL: Final[str] = "https://api.telegram.org/bot$TOKEN/sendMessage"
+
+logging.basicConfig(level=logging.INFO, format="%(levelname)s - %(message)s")
+
+logger = logging.getLogger("OpenWeatherMapTelegramAlert")
+
+
+@dataclass
+class ConfigBase:
+    def update(self, new):
+        for key, value in new.items():
+            if hasattr(self, key):
+                item = getattr(self, key)
+
+                if is_dataclass(item):
+                    item.update(value)
+                else:
+                    setattr(self, key, value)
+
+
+@dataclass
+class OpenWeaterMapConfig(ConfigBase):
+    appid: str = field(default_factory=str)
+    lat: float = field(default_factory=float)
+    lon: float = field(default_factory=float)
+    exclude_tags: list = field(default_factory=list)
+
+
+@dataclass
+class TelegramConfig(ConfigBase):
+    token: str = field(default_factory=str)
+    chat_ids: List[int] = field(default_factory=list)
+
+
+@dataclass
+class Config(ConfigBase):
+    om: OpenWeaterMapConfig = field(default=OpenWeaterMapConfig())
+    telegram: TelegramConfig = field(default=TelegramConfig())
+
+    def __post_init__(self):
+        config_path: Path = Path("/etc/default/om-telegram-alert.yaml")
+
+        _config: dict = self.get_config(config_path)
+        self.update(_config)
+
+    @staticmethod
+    def get_config(config_path: Path) -> dict:
+        _config: dict = {}
+
+        if config_path.exists():
+            _config = yaml.load(config_path.read_text(), Loader=yaml.FullLoader)
+
+        return _config
+
+
+def send_telegram_message(config: Config, text: str):
+    if text:
+        for chat_id in config.telegram.chat_ids:
+            data: dict = {"chat_id": chat_id, "parse_mode": "Markdown", "text": text}
+            r = requests.post(f"https://api.telegram.org/bot{config.telegram.token}/sendMessage", data=data)
+
+            try:
+                r.raise_for_status()
+            except requests.exceptions.HTTPError as e:
+                logger.error("[TELEGRAM] %s", str(e))
+
+
+def check_alerts(config: Config):
+    _temp_filename = Path(gettempdir(), "om-alerts")
+    _temp_alerts: List[dict] = []
+
+    if _temp_filename.exists():
+        _temp_alerts = json.loads(_temp_filename.read_text())
+
+    r = requests.get(
+        f"{OM_ONECALL_URL}?lat={config.om.lat}&lon={config.om.lon}&exclude=current,minutely,hourly,daily&appid={config.om.appid}",
+    )
+
+    try:
+        r.raise_for_status()
+    except requests.exceptions.HTTPError as e:
+        logger.error("[OM] %s", str(e))
+
+    if r.status_code == 200:
+        data: dict = r.json()
+        alerts: List[dict] = data.get("alerts", [])
+
+        # alerts = [
+        #     {
+        #         "sender_name": "ZAMG Zentralanstalt für Meteorologie und Geodynamik",
+        #         "event": "Snow-Icewarning",
+        #         "start": 1643626800,
+        #         "end": 1643842800,
+        #         "description": "Fresh snow up to 80 cm is possible.",
+        #         "tags": ["Snow/Ice"],
+        #     },
+        #     {
+        #         "sender_name": "ZAMG Zentralanstalt für Meteorologie und Geodynamik",
+        #         "event": "Stormwarning",
+        #         "start": 1643720400,
+        #         "end": 1643814000,
+        #         "description": "Gusts of 80 kph are possible.",
+        #         "tags": ["Wind"],
+        #     },
+        # ]
+
+        if alerts:
+            for alert in alerts:
+                if alert in _temp_alerts:
+                    continue
+
+                tags: List[str] = alert["tags"]
+
+                if not any(tag in tags for tag in config.om.exclude_tags):
+                    message: str = (
+                        f"""*{alert["event"]}!*\n`{alert["description"]}`\n\n"""
+                        f"""*Start:* {datetime.utcfromtimestamp(alert["start"]).strftime("%d.%m.%Y, %H:%M")}\n"""
+                        f"""*End:* {datetime.utcfromtimestamp(alert["end"]).strftime("%d.%m.%Y, %H:%M")}\n"""
+                        f"""*Tags*: {",".join(alert["tags"])}\n"""
+                    )
+
+                    send_telegram_message(config, message)
+                    _temp_alerts.append(alert)
+
+            _temp_filename.write_text(json.dumps(_temp_alerts))
+
+
+def main():
+    config = Config()
+    check_alerts(config)
+
+
+if __name__ == "__main__":
+    main()
